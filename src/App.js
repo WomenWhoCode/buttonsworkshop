c77753dd8d43bfd3f844951f9394d17793e78439
Leonor Oliveira <leonorfmartins@gmail.com>
2019-01-27 13:15:41 +0100

    Step 6.1: Adpat handler to be used in all components

--- src/App.js
+++ src/App.js
@@ -18,12 +18,7 @@ class App extends Component {
         Buttons, Buttons!
       </h2>
       <div>
-        <Button color={'green'} onClick={() => this.setColor('green')}>Green</Button>
-        {/* 
-        TODO: Change setColor handler so that it can be reused by all your buttons!
-        hint: We don't want to write the color everytime, but we want instead to use the prop
-        to set the color
-        */}
+        <Button color={'green'} onClick={this.setColor}>Green</Button>
       </div>
     </div> 
     );
--- src/Button.js
+++ src/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
   return (
     <button
       style={{ color: color }}
-      onClick={onClick}
+      onClick={() => onClick(color)}
     >
       {children}
     </button>
